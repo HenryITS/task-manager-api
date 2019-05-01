9fa93939adf4b38d772dbfac86618a8ffd131524
HenryITS <37750623+HenryITS@users.noreply.github.com>
2019-05-01 16:57:53 +0200

    JWT_SECRET

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,11 +5,3 @@ mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true,
     useFindAndModify: false
 })
-
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{    //task-manager-api = db. It is the name of the database.
-//     useNewUrlParser: true, 
-//     useCreateIndex: true,
-//     useFindAndModify: false //to avoid the deprication warning
-//  })
-
